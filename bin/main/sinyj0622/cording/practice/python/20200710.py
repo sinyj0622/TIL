09a0d1fc088feb6832399b95881b4d512b5ce398
eunjeong <euuun22@naver.com>
2020-07-10 16:12:35 +0900

    2020-07-10

---
+++ bin/main/sinyj0622/cording/practice/python/20200710.py
@@ -0,0 +1,20 @@
+# a = [1,2,3]  리스트  생성수정삭제 가능
+# a = (1,2,'a','b')  튜플 변경할 수 없다.
+
+# 자료구조
+# 딕셔너리 (key:value)
+# a = {1:'a', 2:'b', 3:'c'}
+# print(a.keys())
+# print(a.values())
+# print(a.items())
+# for k, v in a.items():
+#     print("키는 " + str(k))
+#     print("값은 " + v)
+# a.clear()
+# print(a)
+# print(a[4])
+# print(a.get(4,'없음'))
+# print(1 in a)
+
+
+
